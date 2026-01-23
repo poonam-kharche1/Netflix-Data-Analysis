ce30d9de4010c2bbab8b3ba5e02644d9983f41a1
poonam-kharche1 <kharche.poonam@gmail.com>
2026-01-23 23:09:07 +0000

    ADD netflix_titles.csv and remove obsolete SQL queries

--- Queries/queries/1.Data Exploration & Cleaning/01_count_movies_vs_tv.sql
+++
@@ -1,7 +0,0 @@
--- Query to count the number of movies vs TV shows in the Netflix Titles dataset
-SELECT 
-type,
-Count(*) as title_count
-From netflix_titles.csv
-Group by type
-Order by title_count DESC;
--- Queries/queries/1.Data Exploration & Cleaning/02_missing_key_fields.sql
+++
@@ -1,9 +0,0 @@
--- Query to select records from the netflix_titles.csv dataset where any of the key fields (show_id, type, title) are missing (NULL).
-SELECT 
-show_id,
-type,
-title
-From netflix_titles.csv
-WHERE show_id IS NULL
-   OR type IS NULL
-   OR title IS NULL;
