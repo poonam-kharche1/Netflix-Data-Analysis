e4b22ec564ea54d2c61d391eb7dda2fc14ded254
poonam-kharche1 <kharche.poonam@gmail.com>
2026-01-24 20:44:04 +0000

    Rename "netflix_titles_countries-Table 1.csv" to "netflix_titles_countries.csv" and add SQL query to find top 10 countries with highest shows including breakdown of movies and TV shows.

---
+++ Queries/queries/02_Top 10 Countries_with_highest_shows.sql
@@ -0,0 +1,15 @@
+-- SQL query to find the top 10 countries with the highest number of shows on Netflix   
+-- The query joins the netflix_titles and countries datasets, counts the number of shows per country,
+-- and orders the results to get the top 10 countries with the most shows.  
+-- It also provides a breakdown of the number of movies and TV shows for each country.
+SELECT
+c.country,
+  COUNT(CASE WHEN nt.type = 'Movie' THEN 1 END) AS movies,
+  COUNT(CASE WHEN nt.type = 'TV Show' THEN 1 END) AS tv_shows,
+COUNT(nt.show_id) AS show_count
+FROM netflix_titles.csv nt
+JOIN netflix_titles_countries.csv c ON nt.show_id = c.show_id
+GROUP BY c.country
+ORDER BY show_count DESC
+LIMIT 1;
+
